be8a384bd68f11f6f3dbdb1d68507ebf7a1d394f
Desiree P <dpricelcsw@gmail.com>
2022-03-20 13:54:32 -0500

    Course update

--- swrapproj/models.py
+++ swrapproj/models.py
@@ -6,9 +6,13 @@ class Course(models.Model):
     course_provider = models.TextField()
     course_completed = models.BooleanField()
     certificate_earned = models.TextField()
+    def __str__(self):
+        return self.course_name, self.course_provider, self.course_completed, self.certificate_earned
 
 class Certification(models.Model):
     certification_name = models.TextField()
     certification_provider = models.TextField()
     date_earned = models.DateField(auto_now_add=True)
     expiration_date = models.DateField()
+    def __str__(self):
+        return self.certification_name, self.certification_provider
--- swrapproj/tests.py
+++ swrapproj/tests.py
@@ -1,3 +1,4 @@
 from django.test import TestCase
+from swrapproj import models
 
 # Create your tests here.
